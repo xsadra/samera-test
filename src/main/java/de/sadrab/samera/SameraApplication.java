b42248eedaf672672dae44f1a8333f1a02e000f2
Sadra <ti9mail@gmail.com>
2018-07-27 11:35:02 +0200

    Add Maven Structure

---
+++ src/main/java/de/sadrab/samera/SameraApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.samera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SameraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SameraApplication.class, args);
+	}
+}
